51d04595fdd7d2be3c2db11a2f5e8a437c5c28cb
Santiago E <santiago.escobar@quipux.com>
2023-03-14 15:42:38 -0500

    web server

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,16 @@
 import store
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return[1,2,3,]
+
+
+@app.get('/contact')
+def get_list():
+    return{'name': 'Platzi'}
 
 def run():
     store.get_categories()
